25bf25f343c2866932fac7a527441d643f4d78e9
Adrienne Rice <adrienne.rice@gmail.com>
2019-02-23 23:28:05 -0500

    complete app

---
+++ clicky-game/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header(props) {
+  return (
+    <div className="header">
+      <h1>Adventure Time Clicky Game!</h1>
+      <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+    </div>
+      );
+}
+
+export default Header;
